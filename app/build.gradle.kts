cfd5655ad43df5fdf89d1677acbd5556d27e6f18
irina <shimkin2014@gmail.com>
2025-01-29 20:42:03 +0300

    1 forma

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapplication"
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
+    fun onClickToast(view: View) {
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast1(view: View) {
+        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast2(view: View) {
+        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast3(view: View) {
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToast4(view: View) {
+        Toast.makeText(this, "Выход", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
