3f42ca24c3d6a4d8cea70975553b445ef506b10f
irina <shimkin2014@gmail.com>
2025-02-25 19:12:11 +0300

    1 forma

---
+++ app/src/main/java/com/example/myapplication/FilmViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.FilmItemBinding
+
+class FilmViewHolder (val binding: FilmItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    private val title = binding.title
+    private val poster = binding.poster
+    private val description = binding.description
+
+    fun bind(film: Film){
+        title.text = film.title
+        poster.setImageResource(film.poster)
+        description.text = film.description
+    }
+
+
+}
