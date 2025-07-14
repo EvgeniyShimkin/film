054276ee8a4a40885f0d47bde631aafb107d9590
irina <shimkin2014@gmail.com>
2025-07-14 15:15:21 +0300

    Переведите получение данных из Room на LiveData. Реализуйте показ ProgressBar, пока загружаются данные из Сети.

---
+++ app/src/main/java/com/example/myapplication/data/DAO/FilmDao.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.data.DAO
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.myapplication.data.Enity.Film
+
+
+@Dao
+interface FilmDao {
+    @Query("SELECT * FROM cached_films")
+    fun getCachedFilms(): LiveData<List<Film>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(list: List<Film>)
+}
